741a18c0cfdf3b95d82018233792e95f53c7d600
EFanZh <efanzh@gmail.com>
2015-03-16 16:13:59 +0800

    Change to semantic HTML tags

--- scripts/common.js
+++ scripts/common.js
@@ -3,12 +3,9 @@
 
 document.addEventListener("DOMContentLoaded", function (e)
 {
-    function $(id)
-    {
-        return document.getElementById(id);
-    }
+    var $ = document.querySelector;
 
-    $("search-form").onsubmit = function (e)
+    $("#search-form").onsubmit = function (e)
     {
         window.location = "https://www.google.com/search?q=" + encodeURIComponent($("search-textbox").value) + " site:{{ site.baseurl }}";
 
